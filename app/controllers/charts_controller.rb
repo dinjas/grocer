b09afc9eb849974fb32cf57b5be54730e2feb405
Jason Dinsmore <jason@dinjas.com>
2017-02-16 17:48:36 -0800

    DRY up charts controller

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,26 +1,40 @@
 class ChartsController < ApplicationController
+
+  before_action :set_baskets,  only: [:monthly_spending,
+                                      :weekly_spending]
+  before_action :set_products, only: [:most_spent, :most_bought]
+  before_action :set_product,  only: [:product_monthly_purchasing,
+                                      :product_weekly_purchasing]
+
   def monthly_spending
-    @baskets = Basket.all
   end
 
   def weekly_spending
-    @baskets = Basket.all
   end
 
   def most_spent
-    @products = Product.all
   end
 
   def most_bought
-    @products = Product.all
   end
 
   def product_monthly_purchasing
-    @product = Product.find(params[:product_id])
   end
 
   def product_weekly_purchasing
+  end
+
+  private
+
+  def set_baskets
+    @baskets = Basket.all
+  end
+
+  def set_product
     @product = Product.find(params[:product_id])
   end
 
+  def set_products
+    @products = Product.all
+  end
 end
