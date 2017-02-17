57054d6a7b332412ebe1dc3b23e1dd5be26e4ad1
Jason Dinsmore <jason@dinjas.com>
2017-02-16 19:38:23 -0800

    Whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,23 @@
 class User < ApplicationRecord
   has_many :baskets
   has_many :line_items, through: :baskets
-  # has_many :products, -> { distinct }, through: :line_items
-  has_many :products,through: :line_items
+  has_many :products, through: :line_items
+
   validates :name, presence: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.email = auth.info.email
-      user.image = auth.info.image
+      user.uid      = auth.uid
+      user.name     = auth.info.name
+      user.email    = auth.info.email
+      user.image    = auth.info.image
+
       if auth.credentials.refresh_token
         user.oauth_refresh_token = auth.credentials.refresh_token
       end
-      user.oauth_token = auth.credentials.token
+
+      user.oauth_token      = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
